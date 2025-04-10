3959ebbda7c3635590981d5bcf7e3138edb92b55
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-10 15:58:42 -0300

    feat: create question controller

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, UseGuards } from '@nestjs/common'
+
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
+import { UserPayload } from 'src/auth/jwt-strategy'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor() {}
+
+  @Post()
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
+  }
+}
