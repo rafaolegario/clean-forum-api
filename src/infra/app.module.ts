6158f2b4b6837a463e5ac527fcc4f074144145d8
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-15 16:02:52 -0300

    feat: remove controller and add http module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './http/controllers/create-account.controller'
+import { PrismaService } from './database/prisma/prisma.service'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './http/controllers/authenticate.controller'
-import { CreateQuestionController } from './http/controllers/create-question.controller'
-import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-questions.controller'
+import { HttpModule } from './http/http.module'
 
 @Module({
   imports: [
@@ -15,13 +12,9 @@ import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-
       isGlobal: true,
     }),
     AuthModule,
+    HttpModule
   ],
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
-  ],
+
   providers: [PrismaService],
 })
 export class AppModule {}
