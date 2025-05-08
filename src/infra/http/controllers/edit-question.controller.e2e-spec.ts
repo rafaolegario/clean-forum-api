03259377c39a0027d767ab5619d5f7c81e0d7620
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-08 15:47:07 -0300

    test: create edit question ctest

---
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Edit question (E2E)', () => {
+  let app: INestApplication
+  let jwt: JwtService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    prisma = moduleRef.get(PrismaService)
+    await app.init()
+  })
+
+  test('[PUT] /questions/:id', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const response = await request(app.getHttpServer())
+      .put(`/questions/${question.id}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'New title',
+        content: 'New content',
+      })
+
+    expect(response.statusCode).toBe(204)
+
+    const EditedQuestion = await prisma.question.findFirst({
+      where: {
+        id: question.id.toString(),
+      },
+    })
+
+    expect(EditedQuestion).toEqual(
+      expect.objectContaining({ title: 'New title' }),
+    )
+  })
+})
