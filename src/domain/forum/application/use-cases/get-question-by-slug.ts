104f0682f2f3e3ed6a9ac5c27014fce3f3a452cf
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-08 11:48:55 -0300

    feat: create get question by slug controller

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -2,6 +2,7 @@ import { QuestionsRepository } from '../repositories/question-repository'
 import { Question } from '../../enterprise/entities/question'
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from './errors/resource-not-found'
+import { Injectable } from '@nestjs/common'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
@@ -14,6 +15,7 @@ type GetQuestionBySlugUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class GetQuestionBySlugUseCase {
   constructor(private QuestionsRepository: QuestionsRepository) {}
 
