929b6d4d0d1502cc0a62ff91eaec0f0a38dfcaeb
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-08 15:43:58 -0300

    feat: Use abstract class

--- src/domain/forum/application/repositories/answer-comment-repository.ts
+++ src/domain/forum/application/repositories/answer-comment-repository.ts
@@ -1,11 +1,11 @@
 import { PaginationParams } from '@/core/repositories/pagination-parms'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-export interface AnswerCommentRepository {
-  create(AnswerComment: AnswerComment): Promise<void>
-  delete(comment: AnswerComment): Promise<void>
-  findById(id: string): Promise<AnswerComment | void>
-  fetchManyCommentsByAnswerId(
+export abstract class AnswerCommentRepository {
+  abstract delete(comment: AnswerComment): Promise<void>
+  abstract create(AnswerComment: AnswerComment): Promise<void>
+  abstract findById(id: string): Promise<AnswerComment | void>
+  abstract fetchManyCommentsByAnswerId(
     AnswerID: string,
     props: PaginationParams,
   ): Promise<AnswerComment[]>
