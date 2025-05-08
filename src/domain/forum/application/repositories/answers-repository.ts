4607a620da2cd7b8b5614812c89d5fa029ebab3d
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-08 15:44:01 -0300

    feat: Use abstract class

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,12 +1,12 @@
 import { PaginationParams } from '@/core/repositories/pagination-parms'
 import { Answer } from '../../enterprise/entities/answer'
 
-export interface AnswersRepository {
-  create(answer: Answer): Promise<void>
-  delete(answer: Answer): Promise<void>
-  save(question: Answer): Promise<void>
-  findById(id: string): Promise<Answer | null>
-  fetchManyByQuestionId(
+export abstract class AnswersRepository {
+  abstract create(answer: Answer): Promise<void>
+  abstract delete(answer: Answer): Promise<void>
+  abstract save(question: Answer): Promise<void>
+  abstract findById(id: string): Promise<Answer | null>
+  abstract fetchManyByQuestionId(
     question: string,
     params: PaginationParams,
   ): Promise<Answer[]>
