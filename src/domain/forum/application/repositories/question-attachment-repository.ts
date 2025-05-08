be26fda40fe15ef00c811934f40b4fe428bf851f
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-08 15:44:04 -0300

    feat: Use abstract class

--- src/domain/forum/application/repositories/question-attachment-repository.ts
+++ src/domain/forum/application/repositories/question-attachment-repository.ts
@@ -1,6 +1,9 @@
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
-export interface QuestionAttachmentsRepository {
-  fetchManyByQuestionId(questionID: string): Promise<QuestionAttachment[]>
-  deleteManyByQuestionId(questionId: string): Promise<void>
+export abstract class QuestionAttachmentsRepository {
+  abstract fetchManyByQuestionId(
+    questionID: string,
+  ): Promise<QuestionAttachment[]>
+
+  abstract deleteManyByQuestionId(questionId: string): Promise<void>
 }
