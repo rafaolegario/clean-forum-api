3fcf7e29002c68525dd10ff13e7365210ae35d8a
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-08 15:47:50 -0300

    feat: create factory make comment answer

--- test/factories/make-comment-answer.ts
+++ test/factories/make-comment-answer.ts
@@ -6,6 +6,9 @@ import {
   AnswerComment,
   AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function MakeCommentAnswer(
   override: Partial<AnswerCommentProps> = {},
@@ -22,4 +25,21 @@ export function MakeCommentAnswer(
   )
 
   return answerComment
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerComment(
+    data: Partial<AnswerCommentProps> = {},
+  ): Promise<AnswerComment> {
+    const answerComment = MakeCommentAnswer(data)
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    })
+
+    return answerComment
+  }
 }
