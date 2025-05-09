758e04300384a4aa203431d0743d4e17431a3f39
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-09 17:30:21 -0300

    test: Create comment on answer test

---
+++ src/infra/http/controllers/comment-on-answer.controller.e2e-spec.ts
@@ -0,0 +1,59 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Comment on answer (E2E)', () => {
+  let app: INestApplication
+  let jwt: JwtService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, AnswerFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    jwt = moduleRef.get(JwtService)
+    await app.init()
+    studentFactory = moduleRef.get(StudentFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+  })
+
+  test('[POST] /answers/:answerId/comments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId: question.id,
+    })
+
+    const response = await request(app.getHttpServer())
+      .post(`/answers/${answer.id}/comments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'Comment Content',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    expect(response.body.answer_comment).toEqual(
+      expect.objectContaining({ content: 'Comment Content' }),
+    )
+  })
+})
