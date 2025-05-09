41095602f3653d2d72bc64972e0c25eedfeebdab
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-09 11:44:19 -0300

    feat: create answer question controller

---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,51 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Post,
+} from '@nestjs/common'
+
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt-strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+import { AnswerPresenter } from '../presenters/answer-presenter'
+
+const answerQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+type AnswerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>
+
+const validationPipe = new ZodValidationPipe(answerQuestionBodySchema)
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @Body(validationPipe) body: AnswerQuestionBodySchema,
+    @Param('questionId') questionId: string,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { content } = body
+    const authorId = user.sub
+    const result = await this.answerQuestion.execute({
+      content,
+      authorId,
+      questionId,
+      attachmentIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    return { answer: AnswerPresenter.toHTTP(result.value.answer) }
+  }
+}
