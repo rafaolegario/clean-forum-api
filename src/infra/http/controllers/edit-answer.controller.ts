2dcbb48b3424b4a10aa4211124a4b0c2bfe9456f
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-09 17:34:14 -0300

    feat: Create edit answer controller

---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,51 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common'
+
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt-strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
+import { AnswerPresenter } from '../presenters/answer-presenter'
+
+const editAnswerBodySchema = z.object({
+  content: z.string(),
+})
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>
+
+const validationPipe = new ZodValidationPipe(editAnswerBodySchema)
+
+@Controller('/answers/:id')
+export class EditAnswerController {
+  constructor(private editAnswer: EditAnswerUseCase) {}
+
+  @Put()
+  @HttpCode(200)
+  async handle(
+    @Body(validationPipe) body: EditAnswerBodySchema,
+    @Param('id') answerId: string,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { content } = body
+    const authorId = user.sub
+    const result = await this.editAnswer.execute({
+      content,
+      authorId,
+      answerId,
+      attachmentIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    return { answer: AnswerPresenter.toHTTP(result.value.answer) }
+  }
+}
