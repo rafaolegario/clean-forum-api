3718690b7797e7328a9216c526fac6409d19563c
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-09 17:30:04 -0300

    test: Create choose question best answer controller

---
+++ src/infra/http/controllers/choose-question-best-answer.controller.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Choose question best answer (E2E)', () => {
+  let app: INestApplication
+  let jwt: JwtService
+  let studentFactory: StudentFactory
+  let answerFactory: AnswerFactory
+  let questionFactory: QuestionFactory
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, AnswerFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    prisma = moduleRef.get(PrismaService)
+    await app.init()
+  })
+
+  test('[PUT] /answers/:answerId/choose-as-best', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId: question.id,
+    })
+
+    const response = await request(app.getHttpServer())
+      .put(`/answers/${answer.id}/choose-as-best`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+
+    const QuestionOnDb = await prisma.question.findFirst({
+      where: {
+        id: question.id.toString(),
+      },
+    })
+
+    expect(QuestionOnDb?.bestAnswerId).toEqual(answer.id.toString())
+  })
+})
