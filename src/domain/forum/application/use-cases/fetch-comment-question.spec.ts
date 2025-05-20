7e8c7372aa113efc9c510b1c0c6add5e4e0ceea4
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-20 15:53:04 -0300

    feat: use in memory student repository

--- src/domain/forum/application/use-cases/fetch-comment-question.spec.ts
+++ src/domain/forum/application/use-cases/fetch-comment-question.spec.ts
@@ -4,15 +4,21 @@ import { FetchCommentQuestionUseCase } from './fetch-comment-question'
 import { MakeCommentQuestion } from 'test/factories/make-comment-question'
 import { InMemoryQuestionRepository } from 'test/repositories/in-memory-questions-repository'
 import { InMemoryQuestionAttachmentRepository } from 'test/repositories/in-memory-questions-attachments-repository'
+import { InMemoryStudentRepository } from 'test/repositories/in-memory-student-repository'
+import { MakeStudent } from 'test/factories/make-student'
 
 let inMemoryQuestionCommentRepository: InMemoryQuestionCommentRepository
 let inMemoryQuestionRepository: InMemoryQuestionRepository
+let inMemoryStudentsRepository: InMemoryStudentRepository
 let sut: FetchCommentQuestionUseCase
 let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository
 
 describe('Fetch question comments', () => {
   beforeEach(() => {
-    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository()
+    inMemoryStudentsRepository = new InMemoryStudentRepository()
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository(
+      inMemoryStudentsRepository,
+    )
     inMemoryQuestionAttachmentRepository =
       new InMemoryQuestionAttachmentRepository()
     inMemoryQuestionRepository = new InMemoryQuestionRepository(
@@ -22,6 +28,9 @@ describe('Fetch question comments', () => {
   })
 
   it('Should be able to fetch comments in a question', async () => {
+    const student = MakeStudent({ name: 'JonhDoe' })
+
+    inMemoryStudentsRepository.items.push(student)
     const question = MakeQuestion()
 
     await inMemoryQuestionRepository.create(question)
@@ -29,6 +38,7 @@ describe('Fetch question comments', () => {
     for (let i = 0; i < 5; i++) {
       const QuestionComment = MakeCommentQuestion({
         questionId: question.id,
+        authorId: student.id,
       })
 
       await inMemoryQuestionCommentRepository.create(QuestionComment)
@@ -39,17 +49,28 @@ describe('Fetch question comments', () => {
       page: 1,
     })
 
-    expect(result.value?.questionComments).toHaveLength(5)
+    expect(result.value?.comments).toHaveLength(5)
+    expect(result.value?.comments).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          authorName: 'JonhDoe',
+        }),
+      ]),
+    )
   })
 
   it('Should be able to paginated comments in a question', async () => {
-    const question = MakeQuestion()
+    const student = MakeStudent({ name: 'JonhDoe' })
+
+    inMemoryStudentsRepository.items.push(student)
+    const question = MakeQuestion({})
 
     await inMemoryQuestionRepository.create(question)
 
     for (let i = 0; i < 22; i++) {
       const QuestionComment = MakeCommentQuestion({
         questionId: question.id,
+        authorId: student.id,
       })
 
       await inMemoryQuestionCommentRepository.create(QuestionComment)
@@ -60,6 +81,6 @@ describe('Fetch question comments', () => {
       page: 2,
     })
 
-    expect(result.value?.questionComments).toHaveLength(2)
+    expect(result.value?.comments).toHaveLength(2)
   })
 })
