68bbd33459b1f463c117576aab71da60df3fc8e4
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-20 15:52:05 -0300

    feat: create fetch with author

--- src/domain/forum/application/repositories/question-comment-repository.ts
+++ src/domain/forum/application/repositories/question-comment-repository.ts
@@ -1,5 +1,6 @@
 import { PaginationParams } from '@/core/repositories/pagination-parms'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentRepository {
   abstract create(QuestionComment: QuestionComment): Promise<void>
@@ -9,4 +10,9 @@ export abstract class QuestionCommentRepository {
     questionID: string,
     props: PaginationParams,
   ): Promise<QuestionComment[]>
+
+  abstract fetchManyCommentsByQuestionIdWithAuthor(
+    questionID: string,
+    props: PaginationParams,
+  ): Promise<CommentWithAuthor[]>
 }
