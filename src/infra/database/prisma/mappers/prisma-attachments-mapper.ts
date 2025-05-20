b4461af506c2583e77692ca8927ffb8eb7e89b80
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-20 15:54:16 -0300

    feat: remove method

--- src/infra/database/prisma/mappers/prisma-attachments-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-attachments-mapper.ts
@@ -1,5 +1,4 @@
 import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
-import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 import { Prisma } from '@prisma/client'
 export class PrismaAttachmentMapper {
   static toPrisma(
@@ -9,25 +8,6 @@ export class PrismaAttachmentMapper {
       id: attachment.id.toString(),
       title: attachment.title,
       url: attachment.url,
-    }
-  }
-
-  static toPrismaUpdatedMany(
-    attachments: QuestionAttachment[],
-  ): Prisma.AttachmentUpdateManyArgs {
-    const attachmentIds = attachments.map((attachment) => {
-      return attachment.attachmentId.toString()
-    })
-
-    return {
-      where: {
-        id: {
-          in: attachmentIds,
-        },
-      },
-      data: {
-        questionId: attachments[0].questionId.toString(),
-      },
     }
   }
 }
