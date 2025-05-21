033b1dbc8da26e50fc3537a6b7c71b55112be0cf
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-21 17:53:18 -0300

    feat: add new method

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -1,11 +1,13 @@
 import { PaginationParams } from '@/core/repositories/pagination-parms'
 import { Question } from '../../enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionsRepository {
   abstract create(question: Question): Promise<void>
   abstract delete(question: Question): Promise<void>
   abstract save(question: Question): Promise<void>
   abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails[]>
   abstract findById(id: string): Promise<Question | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
 }
