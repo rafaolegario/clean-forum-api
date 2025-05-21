da37ee9b9ccb7c758217c521f6662c4162681442
rafaolegario <rafaelbisofficel@gmail.com>
2025-05-21 14:10:52 -0300

    feat create comment with author mapper

---
+++ src/infra/database/prisma/mappers/prisma-comment-with-author-mapper.ts
@@ -0,0 +1,30 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+
+import { Comment as PrismaComment, User as PrismaUser ,Prisma } from '@prisma/client'
+
+type PrismaCommentWithAuthor = PrismaComment & {
+  author: PrismaUser
+}
+
+export class PrismaCommentWithAuthorMapper {
+  static toDomain(raw: PrismaCommentWithAuthor): CommentWithAuthor  {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type')
+    }
+    return CommentWithAuthor.create(
+      {
+        commentId: new UniqueEntityID(raw.id),
+        content: raw.content,
+        authorName: raw.author.name,
+        authorId: new UniqueEntityID(raw.authorId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      
+    )
+  }
+
+  
+}
