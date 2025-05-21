5016f31a4be4ed6fb1972d93fc72872d454bda3e
rafaolegario <rafaelbisofficel@gmail.com>
2025-05-21 14:11:49 -0300

    feat: add new method

--- src/infra/database/prisma/repositories/prisma-question-comment-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comment-repository.ts
@@ -4,6 +4,8 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaQuestionCommentMapper } from '../mappers/prisma-question-comment-mapper'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
 
 @Injectable()
 export class PrismaQuestionCommentRepository
@@ -59,5 +61,26 @@ export class PrismaQuestionCommentRepository
     })
 
     return questionsComment.map(PrismaQuestionCommentMapper.toDomain)
+  }
+
+  async fetchManyCommentsByQuestionIdWithAuthor(
+    questionID: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const questionsComment = await this.prisma.comment.findMany({
+      where: {
+        questionId: questionID,
+      },
+      include:{
+        author:true
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return questionsComment.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 }
