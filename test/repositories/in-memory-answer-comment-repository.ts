0d372d4b4e38e0c81f2e5d9946d89111a0bfd4c0
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-21 17:55:51 -0300

    feat: implement new method

--- test/repositories/in-memory-answer-comment-repository.ts
+++ test/repositories/in-memory-answer-comment-repository.ts
@@ -1,10 +1,14 @@
 import { PaginationParams } from '@/core/repositories/pagination-parms'
 import { AnswerCommentRepository } from '@/domain/forum/application/repositories/answer-comment-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { InMemoryStudentRepository } from './in-memory-student-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export class InMemoryAnswerCommentRepository
   implements AnswerCommentRepository
 {
+  constructor(private inMemoryStudentsRepository: InMemoryStudentRepository){}
+
   public items: AnswerComment[] = []
 
   async create(answerComment: AnswerComment) {
@@ -37,4 +41,35 @@ export class InMemoryAnswerCommentRepository
 
     return comments
   }
+
+
+    async fetchManyCommentsByAnswerIdWithAuthor(
+      answerID: string,
+      { page }: PaginationParams,
+    ) {
+      const comments = this.items
+        .filter((item) => item.answerId.toString() === answerID)
+        .slice((page - 1) * 20, page * 20)
+        .map((comment) => {
+          const author = this.inMemoryStudentsRepository.items.find((student) => {
+            return student.id.equals(comment.authorId)
+          })
+  
+          if (!author) {
+            throw new Error(
+              `Author with ID "${comment.authorId.toString()}" does not exist.`,
+            )
+          }
+  
+          return CommentWithAuthor.create({
+            commentId: comment.id,
+            authorId: comment.authorId,
+            content: comment.content,
+            createdAt: comment.createdAt,
+            authorName: author.name,
+          })
+        })
+  
+      return comments
+    }
 }
