667c1ac514af07ab995100bfd2f129d60430feb3
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-21 17:55:48 -0300

    feat: implement new method

--- src/infra/database/prisma/repositories/prisma-answer-comment-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-comment-repository.ts
@@ -4,6 +4,8 @@ import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaAnswerCommentMapper } from '../mappers/prisma-answer-comment-mapper'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 @Injectable()
 export class PrismaAnswerCommentRepository implements AnswerCommentRepository {
@@ -57,5 +59,26 @@ export class PrismaAnswerCommentRepository implements AnswerCommentRepository {
     })
 
     return answersComment.map(PrismaAnswerCommentMapper.toDomain)
+  }
+
+  async fetchManyCommentsByAnswerIdWithAuthor(
+     answerID: string,
+      { page }: PaginationParams,
+    ): Promise<CommentWithAuthor[]> {
+      const answersComment = await this.prisma.comment.findMany({
+        where: {
+         answerId:answerID,
+        },
+        include:{
+          author:true
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        take: 20,
+        skip: (page - 1) * 20,
+      })
+  
+    return answersComment.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 }
