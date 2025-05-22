1d8133a3e7e23fd472a8fb243e434d574feee615
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-22 16:35:10 -0300

    feat: create notification factory

--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -1,12 +1,15 @@
 import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
-  NoficationProps,
+  NotificationProps,
   Notification,
 } from '@/domain/notification/enterprise/entities/notification'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaNotificationMapper } from '@/infra/database/prisma/mappers/prisma-notification-mapper'
 
 export function MakeNotification(
-  override: Partial<NoficationProps> = {},
+  override: Partial<NotificationProps> = {},
   id?: UniqueEntityID,
 ) {
   const notification = Notification.create(
@@ -20,4 +23,21 @@ export function MakeNotification(
   )
 
   return notification
+}
+
+@Injectable()
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {},
+  ): Promise<Notification> {
+    const notification = MakeNotification(data)
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification),
+    })
+
+    return notification
+  }
 }
