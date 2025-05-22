c3cb27e664b02b1e824e8c408fd5dbb960ce6815
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-22 16:31:29 -0300

    fix imports

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -2,7 +2,7 @@ import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
 import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
-import { SendNotificationUseCase } from '../use-cases/send-notification'
+import { SendNotificationUseCase } from '@/domain/notification/application/use-cases/send-notification'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
@@ -16,8 +16,7 @@ export class OnAnswerCreated implements EventHandler {
 
   setupSubscriptions(): void {
     DomainEvents.register(
-      (event: unknown) =>
-        this.sendNewAnswerNotification(event as AnswerCreatedEvent),
+      this.sendNewAnswerNotification.bind(this),
       AnswerCreatedEvent.name,
     )
   }
@@ -29,9 +28,10 @@ export class OnAnswerCreated implements EventHandler {
 
     if (question) {
       await this.sendNotification.execute({
-        recipientId: question?.authorId.toString(),
-        title:
-          'new answer in ' + question?.title.substring(0, 40).concat('...'),
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em "${question.title
+          .substring(0, 40)
+          .concat('...')}"`,
         content: answer.excerpt,
       })
     }
