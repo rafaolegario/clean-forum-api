3edf50b7dbad7e90432f43c8849e9faeda0273be
rafaolegario <rafaelbisofficel@gmail.com>
2025-05-22 11:05:33 -0300

    refactor: add null return

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -7,7 +7,7 @@ export abstract class QuestionsRepository {
   abstract delete(question: Question): Promise<void>
   abstract save(question: Question): Promise<void>
   abstract findBySlug(slug: string): Promise<Question | null>
-  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails[]>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract findById(id: string): Promise<Question | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
 }
