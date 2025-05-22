abe6b4154237269dd3f244bf83eddcbbdf9d0a21
rafaolegario <rafaelbisofficel@gmail.com>
2025-05-22 11:10:02 -0300

    feat: create toDomain in mapper

--- src/infra/database/prisma/mappers/prisma-attachments-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-attachments-mapper.ts
@@ -1,13 +1,24 @@
-import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
-import { Prisma } from '@prisma/client'
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Attachment } from "@/domain/forum/enterprise/entities/attachment";
+import { Attachment as PrismaAttachment, Prisma } from "@prisma/client";
 export class PrismaAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): Attachment {
+    return Attachment.create(
+      {
+        title: raw.title,
+        url: raw.url,
+      },
+      new UniqueEntityID(raw.id)
+    );
+  }
+
   static toPrisma(
-    attachment: Attachment,
+    attachment: Attachment
   ): Prisma.AttachmentUncheckedCreateInput {
     return {
       id: attachment.id.toString(),
       title: attachment.title,
       url: attachment.url,
-    }
+    };
   }
 }
